829141ae35b2d6dd28bd20cf011d4baf1575ee49
no-relation <eddiec76@gmail.com>
2018-11-30 13:24:32 -0600

    delete fixed, playing with likes, opimistic rendering

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
   let imageId = 1549 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
-
+  
   const image = document.getElementById('image')
   const name = document.getElementById('name')
   const likes = document.getElementById('likes')
-  const likeButton = document.getElementById('like_button')
+  const upvote = document.getElementById('like_button')
+  const downvote = document.getElementById('dislike_button')
+  
   const commentForm = document.getElementById('comment_form')
   const commentInput = document.getElementById('comment_input')
   const commentList = document.getElementById('comments')
@@ -29,46 +29,80 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function render() {
-    console.log(imgObject)
+    renderImage()
+    renderLikes()
+    renderComments()
+    
+    upvote.addEventListener('click', ()=>{addLike(true)})
+    downvote.addEventListener('click', ()=>{addLike(false)})
+    commentForm.addEventListener('submit', submitComment)
+  }
+
+  const renderImage = function() {  
     image.setAttribute('src', imgObject.url)
     name.innerHTML = imgObject.name
+  }
+  
+  const renderLikes = function() {
     likes.innerHTML = imgObject.like_count
-    likeButton.addEventListener('click', addLike)
-    commentForm.addEventListener('submit', submitComment)
-
-    imgObject.comments.forEach((comment)=>{
-      // commentList.innerHTML = ''
-      const commentItem = document.createElement('li')
-      commentItem.innerHTML = comment.content
-      commentList.append(commentItem)
-    })
   }
 
-  function addLike() {
-    imgObject.like_count++
-    updateLikes(imgObject.id)
+  const renderComments = function() {
+  commentList.innerHTML = ''
+  imgObject.comments.forEach((comment) => {
+    const commentItem = document.createElement('li')
+    commentItem.innerHTML = comment.content
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = 'x'
+
+    deleteButton.addEventListener('click', () => { deleteComment(comment.id) })
+
+    commentItem.append(deleteButton)
+    commentList.append(commentItem)
+  })
+}
+
+  function addLike(bool) {
+    if (bool) {
+      imgObject.like_count++
+      updateLikes(imgObject.id, true)
+    } else {
+      imgObject.like_count--
+      updateLikes(imgObject.id, false)
+    }
+    renderLikes()
   }
 
-  function updateLikes(imageId) {
-    fetch(likeURL, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        image_id: imageId
+  function updateLikes(imageId, isUp) {
+    if (isUp) {
+      fetch(likeURL, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          image_id: imageId
+        })
       })
-    })
-    .then(fetchImg)
+    } else {
+      // no route implemented for randopic API
+      // fetch(likeURL, {
+      //   method: 'DELETE'
+      // })
+    }
   }
 
-  function submitComment() {
+  function submitComment(e) {
+    e.preventDefault()
     newComment = {
       "image_id": imgObject.id,
       "content": commentInput.value
     }
     imgObject.comments.push(newComment)
+    commentInput.value = ''
+    console.log(imgObject)
+    renderComments()
     updateComments(newComment)
   }
 
@@ -83,6 +117,12 @@ document.addEventListener('DOMContentLoaded', () => {
         image_id: comment.image_id,
         content: comment.content
       })
+    })
+  }
+
+  function deleteComment(commentID) {
+    fetch(commentsURL + commentID, {
+      method: 'DELETE'
     })
     .then(fetchImg)
   }
