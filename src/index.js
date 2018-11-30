ad348c95927019cf358ef99186e704b783818d3f
no-relation <eddiec76@gmail.com>
2018-11-30 10:20:49 -0600

    fetch is happening

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 1549 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  let imgObject;
+
+  function fetchImg()  {
+    fetch(imageURL)
+    .then((response)=>response.json())
+    .then((data)=>{
+      imgObject = data
+      render()
+  })
+
+  function render() {
+    
+  }
+
+
+
+  fetchImg()
 })
