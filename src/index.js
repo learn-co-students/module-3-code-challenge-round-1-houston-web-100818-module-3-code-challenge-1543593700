9e091a73f66fa52cfd59ba6369a7ba15df248af0
ch0mper <hannahchang17@gmail.com>
2018-11-30 12:12:57 -0600

    no delete button yet

--- src/index.js
+++ src/index.js
@@ -9,9 +9,23 @@ const likeButton = document.querySelector('#like_button')
 const commentsList = document.querySelector('#comments')
 const commentField = document.querySelector('#comment_input')
 const submitButton = document.querySelector('#submit-button')
+let deleteButton
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('🦜 %c DOM Content Loaded and Parsed! 🦜', 'color: magenta')
+  likeButton.style.margin = '3px'
+  likeButton.addEventListener('mouseover', (e) => {
+    likeButton.innerText = 'Like 👍'
+  })
+  likeButton.addEventListener('mouseout', (e) => {
+    likeButton.innerText = 'Like'
+  })
+  submitButton.addEventListener('mouseover', (e) => {
+    submitButton.value = 'Submit 🦜'
+  })
+  submitButton.addEventListener('mouseout', (e) => {
+    submitButton.value = 'Submit'
+  })
   fetchImg()
 })
 
@@ -28,13 +42,6 @@ const render = (i) => {
   name.innerText = i.name
   likes.innerText = `${i.like_count}`
 
-  likeButton.style.margin = '3px'
-  likeButton.addEventListener('mouseover', (e) => {
-    likeButton.innerText = 'Like 👍'
-  })
-  likeButton.addEventListener('mouseout', (e) => {
-    likeButton.innerText = 'Like'
-  })
   likeButton.addEventListener('click', (e) => {
     i.like_count++
     addLike(i)
@@ -45,14 +52,10 @@ const render = (i) => {
     let listItem = document.createElement('li')
     listItem.innerText = c.content
     commentsList.append(listItem)
+    makeDeleteButton(listItem)
+    listItem.append(deleteButton)
   })
 
-  submitButton.addEventListener('mouseover', (e) => {
-    submitButton.value = 'Submit 🦜'
-  })
-  submitButton.addEventListener('mouseout', (e) => {
-    submitButton.value = 'Submit'
-  })
   submitButton.addEventListener('click', (e) => {
     e.preventDefault()
     addComment()
@@ -74,12 +77,45 @@ const addLike = (i) => {
 }
 
 const addComment = () => {
-  let newComment = document.createElement('li')
   let commentText = commentField.value
-  newComment.innerHTML = `✨✨<b>'${commentText}'</b> added ✨✨`
-  commentsList.append(newComment)
-  commentField.value = ''
-  setTimeout( () => {
-    newComment.innerHTML = `${commentText}`
-  }, 2000)
+  if (commentText != '') {
+    let listItem = document.createElement('li')
+    listItem.innerHTML = `✨✨<b>"${commentText}"</b> added ✨✨`
+    commentsList.append(listItem)
+    commentField.value = ''
+    commentField.placeholder="Add MOAR Comments"
+    setTimeout( () => {
+      listItem.innerHTML = `${commentText}`
+    }, 1200)
+    setTimeout( () => {
+      makeDeleteButton(listItem)
+      listItem.append(deleteButton)
+    }, 1210)
+    fetch('https://randopic.herokuapp.com/comments', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: imageId,
+        content: commentText
+      })
+    })
+  }
+  else {alert('please type in your comment, we are not psychic 🔮')}
+}
+
+const makeDeleteButton = (comment) => {
+  deleteButton = document.createElement('button')
+  deleteButton.innerText = 'X'
+  deleteButton.style.width = '20px'
+  deleteButton.style.padding = '1px'
+  deleteButton.style.margin = '10px'
+  deleteButton.style.background = 'rgb(247, 150, 150)'
+  deleteButton.addEventListener('click', (e) => {
+    fetch(`https://randopic.herokuapp.com/comments/${comment.id}`, {
+    method: 'DELETE'})
+    .then(fetchImg)
+  })
 }
