30fdb77a85662cece304f59aa0c92092c29393ff
no-relation <eddiec76@gmail.com>
2018-11-30 10:34:45 -0600

    page rendered

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const image = document.getElementById('image')
+  const name = document.getElementById('name')
+  const likes = document.getElementById('likes')
+  const likeButton = document.getElementById('like_button')
+  const commentForm = document.getElementById('comment_form')
+  const commentInput = document.getElementById('comment_input')
+  const commentList = document.getElementById('comments')
+
   let imgObject;
 
   function fetchImg()  {
@@ -17,10 +25,19 @@ document.addEventListener('DOMContentLoaded', () => {
     .then((data)=>{
       imgObject = data
       render()
-  })
+    })
+  }
 
   function render() {
-    
+    image.setAttribute('src', imgObject.url)
+    name.innerHTML = imgObject.name
+    likes.innerHTML = imgObject.like_count
+
+    imgObject.comments.forEach((comment)=>{
+      const commentItem = document.createElement('li')
+      commentItem.innerHTML = comment.content
+      commentList.append(commentItem)
+    })
   }
 
 
